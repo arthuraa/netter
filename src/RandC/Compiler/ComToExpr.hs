aa21ed7e1b4611c266866a56a8fa07e3bfb83021
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2020-02-03 16:03:56 -0500

    Extract ComToExpr pass.

---
+++ src/RandC/Compiler/ComToExpr.hs
@@ -0,0 +1,30 @@
+module RandC.Compiler.ComToExpr where
+
+import RandC.Var
+import RandC.P
+import qualified RandC.Dice.Expr as E
+import RandC.Dice
+
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+-- Transform a command into an equivalent mapping of parallel assignments.
+-- If m = compile c, then running v := val m v should have the same effect
+-- on v as running c.
+compile :: Com -> M.Map Var E.Expr
+compile Skip         = M.empty
+compile (Assn v e)   = M.singleton v e
+compile (Seq c1 c2)  = let m1  = compile c1
+                           m2  = M.map (E.subst $ E.at m1) $ compile c2 in
+                         M.unionWith (\e1 e2 -> e2) m1 m2
+compile (If e c1 c2) = let m1 = compile c1
+                           m2 = compile c2
+                           vs = M.keysSet m1 `S.union` M.keysSet m2 in
+                         M.fromSet (\v -> E.If e (m1 `E.at` v) (m2 `E.at` v)) vs
+
+-- The specification for compile, replacing the binding map by a function.
+compileF :: Com -> Var -> E.Expr
+compileF Skip               v = E.Var v
+compileF (Assn v' e)        v = if v == v' then e else E.Var v
+compileF (Seq c1 c2)        v = E.subst (compileF c1) (compileF c2 v)
+compileF (If e cThen cElse) v = E.If e (compileF cThen v) (compileF cElse v)
--- src/RandC/Dice.hs
+++ src/RandC/Dice.hs
@@ -19,24 +19,3 @@ data Program = Program { pVarDecls :: M.Map Var (Int, Int)
                        , pDieDecls :: M.Map DE.Die [Double]
                        , pCom :: Com }
   deriving (Show, Eq)
-
--- Transform a command into an equivalent mapping of parallel assignments.
--- If m = flatten c, then running v := val m v should have the same effect
--- on v as running c.
-flatten :: Com -> M.Map Var DE.Expr
-flatten Skip         = M.empty
-flatten (Assn v e)   = M.singleton v e
-flatten (Seq c1 c2)  = let m1  = flatten c1
-                           m2  = M.map (DE.subst $ DE.at m1) $ flatten c2 in
-                         M.unionWith (\e1 e2 -> e2) m1 m2
-flatten (If e c1 c2) = let m1 = flatten c1
-                           m2 = flatten c2
-                           vs = M.keysSet m1 `S.union` M.keysSet m2 in
-                         M.fromSet (\v -> DE.If e (m1 `DE.at` v) (m2 `DE.at` v)) vs
-
--- The specification for flatten, replacing the binding map by a function.
-flattenF :: Com -> Var -> DE.Expr
-flattenF Skip               v = DE.Var v
-flattenF (Assn v' e)        v = if v == v' then e else DE.Var v
-flattenF (Seq c1 c2)        v = DE.subst (flattenF c1) (flattenF c2 v)
-flattenF (If e cThen cElse) v = DE.If e (flattenF cThen v) (flattenF cElse v)
