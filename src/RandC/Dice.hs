a65401770805801019bbd1b2e197648b9bee5cbe
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2020-02-26 17:34:46 -0500

    Pretty printer for dice.

--- src/RandC/Dice.hs
+++ src/RandC/Dice.hs
@@ -1,6 +1,7 @@
 module RandC.Dice where
 
 import RandC.Var
+import RandC.Formatting
 import RandC.Prob
 import qualified RandC.Prism.Expr as PE
 
@@ -18,5 +19,24 @@ data Program = Program { pVarDecls :: M.Map Var (Int, Int)
                        , pCom :: Com }
   deriving (Show, Eq)
 
+instance Pretty Com where
+  pretty Skip          = pretty "skip"
+  pretty (Assn v e)    = sep [ pretty v, pretty ".<-", pretty e ]
+  pretty (Seq c1 c2)   = vcat [ pretty c1 <> pretty ";"
+                              , pretty c2 ]
+  pretty (If e c1 c2)  = vcat [ sep [ pretty "if", pretty e, pretty "then" ]
+                              , indent 2 (pretty c1)
+                              , pretty "else"
+                              , indent 2 (pretty c2) ]
+
 instance Pretty Program where
-  pretty = viaShow
+  pretty (Program decls dice com) =
+    vcat [ pretty "vars"
+         , vcat [ sep [ pretty v, interval lb ub ]
+                | (v, (lb, ub)) <- M.assocs decls ]
+         , line
+         , pretty "dice"
+         , vcat [ sep [ pretty d, pretty ps ]
+                | (d, ps) <- M.assocs dice ]
+         , line
+         , pretty com ]
