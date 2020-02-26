90837ebfd3c37b92f6e5234aa87f6192d47f4e7a
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2020-02-26 13:15:30 -0500

    Pretty printer for imp.

--- src/RandC/Imp.hs
+++ src/RandC/Imp.hs
@@ -1,8 +1,9 @@
 module RandC.Imp where
 
 import RandC.Var
+import RandC.Formatting
 import RandC.Prism.Expr hiding (If)
-import RandC.Prob
+import RandC.Prob hiding (Choice)
 
 import Data.Text.Prettyprint.Doc
 import qualified Data.Map.Strict as M
@@ -23,5 +24,19 @@ revSeq [] = Skip
 revSeq [c] = c
 revSeq (c : coms) = Seq (revSeq coms) c
 
+instance Pretty Com where
+  pretty Skip          = pretty "skip"
+  pretty (Assn v e)    = sep [ pretty v, pretty ".<-", pretty e ]
+  pretty (Seq c1 c2)   = vcat [ pretty c1 <> pretty ";"
+                              , pretty c2 ]
+  pretty (If e c1 c2)  = vcat [ sep [ pretty "if", pretty e, pretty "then" ]
+                              , indent 2 (pretty c1)
+                              , pretty "else"
+                              , indent 2 (pretty c2) ]
+  pretty (Choice v es) = sep [ pretty v, pretty ".<-$", pretty es ]
+
 instance Pretty Program where
-  pretty = viaShow
+  pretty (Program decls com) =
+    vcat [ vcat [ sep [ pretty v, interval lb ub ]
+                | (v, (lb, ub)) <- M.assocs decls ]
+         , pretty com ]
