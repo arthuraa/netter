b23db6cc4126909d486f5c85db9dd67b332ed101
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2020-03-05 17:00:12 -0500

    Program simplifier.

--- src/RandC/Compiler/Optimize.hs
+++ src/RandC/Compiler/Optimize.hs
@@ -5,9 +5,11 @@ import RandC.Imp
 import RandC.Prob       hiding (resolve)
 import RandC.Pass
 import qualified RandC.Prism.Expr as PE
-import RandC.Prism.Expr hiding (If)
+import RandC.Prism.Expr hiding (If, simplify)
+import qualified RandC.Options as O
 
 import Control.Monad.State
+import Control.Monad.Reader
 import Data.Maybe (isNothing)
 import qualified Data.Set        as S
 import qualified Data.Map.Strict as M
@@ -68,12 +70,38 @@ evalInstr (If e cThen cElse) = do
 
   return $ If e cThen cElse
 
-extractLocals :: Program -> Pass Program
-extractLocals (Program decls defs com) = do
+eval :: Program -> Pass Program
+eval (Program decls defs com) = do
   (Com is, es') <- runStateT (evalCom com) M.empty
   let is' = if es' == M.empty then is
             else is ++ [Assn $ M.map return es']
   return $ Program decls defs $ Com $ is'
 
+simplify :: Program -> Program
+simplify (Program decls defs com) =
+  Program decls (M.map PE.simplify defs) (simplifyCom com)
+
+simplifyCom :: Com -> Com
+simplifyCom (Com is) = Com $ simplifyInstrs is
+
+simplifyInstrs :: [Instr] -> [Instr]
+simplifyInstrs [] = []
+simplifyInstrs (i : is) =
+  case (simplifyInstr i, simplifyInstrs is) of
+    (i'@(Assn assn), is') ->
+      if assn == M.empty then is' else i' : is'
+    (i'@(If _ cThen cElse), is') ->
+      if cThen == cElse then instrs cThen ++ is'
+      else i' : is'
+
+simplifyInstr :: Instr -> Instr
+simplifyInstr (Assn assns) =
+  Assn (M.map (fmap PE.simplify) assns)
+simplifyInstr (If e cThen cElse) =
+  If (PE.simplify e) (simplifyCom cThen) (simplifyCom cElse)
+
 optimize :: Program -> Pass Program
-optimize prog = extractLocals prog
+optimize prog = do
+  evProg <- eval prog
+  doSimplify <- reader O.simplify
+  if doSimplify then return $ simplify evProg else return evProg
