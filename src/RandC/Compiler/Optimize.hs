c76676b1676179dac026b0214015fdf8d8127e1f
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2020-03-11 23:09:18 -0400

    Improve merging.

--- src/RandC/Compiler/Optimize.hs
+++ src/RandC/Compiler/Optimize.hs
@@ -3,6 +3,7 @@ module RandC.Compiler.Optimize where
 import RandC.Var
 import RandC.Imp
 import RandC.Prob       hiding (resolve)
+import RandC.G          hiding (If)
 import RandC.Pass
 import qualified RandC.Prism.Expr as PE
 import RandC.Prism.Expr hiding (If, simplify)
@@ -17,6 +18,59 @@ assnDeps assn =
   foldl addVarDeps S.empty assn
   where addVarDeps allDeps es = foldl addExprDeps allDeps es
         addExprDeps allDeps e = S.union allDeps (vars e)
+
+type Deps = M.Map Var (S.Set Var)
+
+-- Compute the dependencies of intermediate definitions on state variables
+defDeps :: M.Map Var Expr -> Deps
+defDeps defs = foldl updateDependencies M.empty $ M.keysSet defs
+  where directDepMap = M.map vars defs
+        updateDependencies deps v
+          | M.member v deps =
+            -- The variable v has already been visited, so we can skip it
+            deps
+          | otherwise =
+            let directDeps = M.findWithDefault S.empty v directDepMap
+                toVisit    = S.difference directDeps (M.keysSet deps)
+                stateDeps  = S.difference directDeps (M.keysSet defs)
+                deps'      = foldl updateDependencies deps toVisit
+                allDeps    = S.unions
+                             $ stateDeps
+                             : [ M.findWithDefault S.empty v' deps'
+                               | v' <- S.toList directDeps ] in
+              M.insert v allDeps deps'
+
+exprDep :: Deps -> Expr -> S.Set Var
+exprDep deps e = S.unions [ M.findWithDefault (S.singleton v) v deps
+                          | v <- S.toList $ vars e ]
+
+probExprDep :: Deps -> P Expr -> S.Set Var
+probExprDep deps e = S.unions $ fmap (exprDep deps) e
+
+guardedExprDep :: Deps -> G (P Expr) -> S.Set Var
+guardedExprDep deps e = S.unions $ fmap (probExprDep deps) e
+
+mergeInstrs :: Deps -> [Instr] -> [Instr]
+mergeInstrs deps is = go is
+  where go [] = []
+        go (i : is) =
+          case (i, go is) of
+            (i@(Assn assn), i'@(Assn assn') : is') ->
+              let assnVars  = M.keysSet assn
+                  assnVars' = M.keysSet assn'
+                  assnDeps' = S.unions $ fmap (probExprDep deps) assn' in
+                if S.disjoint assnVars assnDeps' &&
+                   S.disjoint assnVars assnVars' then
+                  Assn (M.union assn assn') : is'
+                else i : i' : is'
+            (i, is) -> i : is
+
+mergeCom :: Deps -> Com -> Com
+mergeCom deps (Com is) = Com $ mergeInstrs deps is
+
+merge :: Program -> Program
+merge (Program decl defs blocks) =
+  Program decl defs $ mergeCom (defDeps defs) blocks
 
 simplify :: Program -> Program
 simplify (Program decls defs com) =
@@ -51,4 +105,5 @@ maybeOptimize opt f prog = do
 
 optimize :: Program -> Pass Program
 optimize =
+  maybeOptimize O.merge (return . merge) >=>
   maybeOptimize O.simplify (return . simplify)
